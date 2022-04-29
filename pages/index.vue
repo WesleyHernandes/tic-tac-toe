2889f7eb2f8ebed9db16ce875ab86fad121140c9
WesleyMarknet <wess.hernandes@gmail.com>
2022-04-29 19:58:52 -0300

    Estrutura inicial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="postcss" scoped>
+  *{
+    @apply
+    m-0
+    p-0
+    box-border;
+  }
+</style>
